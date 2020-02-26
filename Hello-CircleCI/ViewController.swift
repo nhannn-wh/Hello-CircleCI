b2f22acdc13e5144c78c8224e63f9685b6eccecc
NhanWorkHouse <nhan.nn@workhouse.me>
2020-02-26 15:01:10 +0700

    + Test Log

--- Hello-CircleCI/ViewController.swift
+++ Hello-CircleCI/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        NSLog("nhan")
         // Do any additional setup after loading the view.
     }
 
